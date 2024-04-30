050f70c5a42233814409abe4e79c6692f19105b3
omar salem <omar.gamil.salem@gmail.com>
2024-04-30 20:53:31 +1000

    -deployment

--- run_robot.sh
+++ run_robot.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 mkdir -p ugv_ws/src
-cd $_
+cd ugv_ws || exit
+(
+cd src || exit
 git clone https://github.com/Omar-Salem/ugv.git
-
-mv two_wheels/* .
-rm -rf two_wheels
-cd ..
+)
 rm -rf build/ install/ log/
 colcon build
-source install/setup.bash && ros2 launch two_wheels_core two_wheels.launch.py
+source install/setup.bash
+ros2 launch ugv_control control.launch.py
