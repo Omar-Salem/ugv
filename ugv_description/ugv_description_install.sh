d5b119feafc618f4c267330591c9290486d00877
omar salem <omar.gamil.salem@gmail.com>
2024-04-30 18:23:56 +1000

    -common macro file

--- ugv_description/ugv_description_install.sh
+++ ugv_description/ugv_description_install.sh
@@ -2,3 +2,8 @@
 colcon build --packages-select ugv_description
 source install/setup.bash
 ros2 launch ugv_description display.launch.py
+
+
+colcon build --packages-select ugv_description
+source install/setup.bash
+ros2 launch ugv_description gazebo.launch.py
