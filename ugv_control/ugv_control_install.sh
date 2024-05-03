89df9237690018467836bd0910452835c1c64679
omar salem <omar.gamil.salem@gmail.com>
2024-05-03 14:53:16 +1000

    -ros2 control test

--- ugv_control/ugv_control_install.sh
+++ ugv_control/ugv_control_install.sh
@@ -7,3 +7,8 @@ ros2 launch ugv_control control.launch.py
 colcon build --packages-select ugv_control
 source install/setup.bash
 ros2 launch ugv_control gazebo_control.launch.py
+
+
+ros2 topic pub -r 10 /diff_drive_controller/cmd_vel_unstamped geometry_msgs/msg/Twist "
+  linear:
+    x: 0.5"
