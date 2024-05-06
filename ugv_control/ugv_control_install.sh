8989814667f1f9fb29a40efde9448fd6fbf48911
omar salem <omar.gamil.salem@gmail.com>
2024-05-06 20:47:19 +1000

    nav

--- ugv_control/ugv_control_install.sh
+++ ugv_control/ugv_control_install.sh
@@ -13,4 +13,6 @@ ros2 launch ugv_control gazebo.launch.py
 
 ros2 topic pub -r 10 /diff_drive_controller/cmd_vel_unstamped geometry_msgs/msg/Twist "
   linear:
-    x: 0.5"
+    x: 20"
+
+ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r cmd_vel:=/diff_drive_controller/cmd_vel_unstamped
---
+++ ugv_nav/ugv_nav_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/ugv_ws
+colcon build --packages-select ugv_nav
+source install/setup.bash
+ros2 launch ugv_nav nav.launch.py
+
+cd ~/ugv_ws
+colcon build --packages-select ugv_nav
+source install/setup.bash
+ros2 launch ugv_nav gazebo.launch.py
