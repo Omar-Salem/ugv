f84eb47008f5be060725998116d96bab5cb73081
omar salem <omar.gamil.salem@gmail.com>
2024-05-07 13:56:37 +1000

    -cd to workspace

--- ugv_description/ugv_description_install.sh
+++ ugv_description/ugv_description_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
+cd ~/ugv_ws
 colcon build --packages-select ugv_description
 source install/setup.bash
 ros2 launch ugv_description display.launch.py
 
-
+cd ~/ugv_ws
 colcon build --packages-select ugv_description
 source install/setup.bash
 ros2 launch ugv_description gazebo.launch.py
