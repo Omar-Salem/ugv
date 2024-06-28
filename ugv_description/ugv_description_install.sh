e79ca9af9360a212bb16d72335e045da4fcdf0b1
omar salem <omar.gamil.salem@gmail.com>
2024-06-28 11:28:54 +1000

    lidar urdf

--- ugv_description/ugv_description_install.sh
+++ ugv_description/ugv_description_install.sh
@@ -2,7 +2,4 @@
 
 cd ~/ugv_ws && colcon build --packages-select ugv_description && source install/setup.bash && ros2 launch ugv_description display.launch.py
 
-cd ~/ugv_ws
-colcon build --packages-select ugv_description
-source install/setup.bash
-ros2 launch ugv_description gazebo.launch.py
+cd ~/ugv_ws && colcon build --packages-select ugv_description && source install/setup.bash && ros2 launch ugv_description gazebo.launch.py
