be32c274209f2153da0dc7586395b50cd4ab8419
omar salem <omar.gamil.salem@gmail.com>
2024-06-28 12:00:34 +1000

    fixed gazebo nodes

--- ugv_description/launch/gazebo.launch.py
+++ ugv_description/launch/gazebo.launch.py
@@ -7,13 +7,16 @@ from launch.substitutions import PathJoinSubstitution
 import os
 import xacro
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration
 
 
 def generate_launch_description():
     share_dir = get_package_share_directory('ugv_description')
 
     xacro_file = os.path.join(share_dir, 'urdf', 'ugv.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    robot_description_config = xacro.process_file(xacro_file, mappings={'is_sim': 'true'})
     robot_urdf = robot_description_config.toxml()
 
     robot_state_publisher_node = Node(
@@ -34,28 +37,6 @@ def generate_launch_description():
         name='joint_state_publisher'
     )
 
-    gazebo_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzserver.launch.py'
-            ])
-        ]),
-        launch_arguments={
-            'pause': 'true'
-        }.items()
-    )
-
-    gazebo_client = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzclient.launch.py'
-            ])
-        ])
-    )
 
     urdf_spawn_node = Node(
         package='gazebo_ros',
@@ -75,10 +56,30 @@ def generate_launch_description():
         output='screen'
     )
     return LaunchDescription([
+            DeclareLaunchArgument(
+                name='gazebo_world',
+                default_value=''
+            ),
         robot_state_publisher_node,
         joint_state_publisher_node,
-        gazebo_server,
-        gazebo_client,
         urdf_spawn_node,
         rviz_node
-    ])
+    ]+create_gazebo_nodes())
+    
+def create_gazebo_nodes() -> list:
+    """
+
+    :rtype: list
+    """
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={'world': LaunchConfiguration('gazebo_world')}.items()
+    )
+    spawn_entity = Node(package='gazebo_ros',
+                        executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'ugv'],
+                        output='screen')
+    # launch.logging.launch_config.level = logging.WARN
+    return [gazebo, spawn_entity]
