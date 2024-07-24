bb1e49adce11170d2fc98b9cdc3141bb12a402cf
Omar Salem <omar.gamil.salem@gmail.com>
2024-07-24 16:06:16 +1000

    migrate to gazebo harmonic

--- ugv_description/launch/gazebo.launch.py
+++ ugv_description/launch/gazebo.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
 
     xacro_file = os.path.join(share_dir, 'urdf', 'ugv.xacro')
     robot_description_config = xacro.process_file(xacro_file, mappings={'is_sim': 'true'})
-    robot_urdf = robot_description_config.toxml()
+    robot_urdf = robot_description_config.toprettyxml(indent='  ')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -36,17 +36,6 @@ def generate_launch_description():
         executable='joint_state_publisher',
         name='joint_state_publisher'
     )
-
-
-    urdf_spawn_node = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=[
-            '-entity', 'ugv',
-            '-topic', 'robot_description'
-        ],
-        output='screen'
-    )
     rviz_config_file = os.path.join(share_dir, 'config', 'display.rviz')
     rviz_node = Node(
         package='rviz2',
@@ -57,29 +46,87 @@ def generate_launch_description():
     )
     return LaunchDescription([
             DeclareLaunchArgument(
-                name='gazebo_world',
-                default_value=''
+                name='world',
+                default_value=os.path.join(share_dir, 'worlds', 'fws_robot_world.sdf')
             ),
         robot_state_publisher_node,
         joint_state_publisher_node,
-        urdf_spawn_node,
         rviz_node
-    ]+create_gazebo_nodes())
+    ]+create_gazebo_nodes(robot_urdf))
+
+def create_gazebo_nodes(robot_urdf) -> list:
+    """
+
+    :rtype: list
+    """    
     
-def create_gazebo_nodes() -> list:
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+         
+             )
+
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-string', robot_urdf,
+                   '-x', '0.0',
+                   '-y', '0.0',
+                   '-z', '0.07',
+                   '-R', '0.0',
+                   '-P', '0.0',
+                   '-Y', '0.0',
+                   '-name', 'fws_robot',
+                   '-allow_renaming', 'false'],
+    )
+
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'],
+        output='screen'
+    )
+
+    return [gazebo,gz_spawn_entity,bridge]
+
+# def create_gazebo_nodes(robot_urdf) -> list:
     """
 
     :rtype: list
     """
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-        launch_arguments={'world': LaunchConfiguration('gazebo_world')}.items()
+    ros_gz_sim =get_package_share_directory('ros_gz_sim')
+    gzserver_cmd = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    ros_gz_sim, 'launch'), '/gz_sim.launch.py']),
+    launch_arguments={'gz_args': ['-v4 ', LaunchConfiguration('gazebo_world')], 'on_exit_shutdown': 'true'}.items()
+             )
+    gzclient_cmd = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+        os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+    ),
+    launch_arguments={'gz_args': '-g -v4 '}.items()
+)
+
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-string', robot_urdf,
+                   '-x', '0.0',
+                   '-y', '0.0',
+                   '-z', '0.07',
+                   '-R', '0.0',
+                   '-P', '0.0',
+                   '-Y', '0.0',
+                   '-name', 'ugv',
+                   '-allow_renaming', 'false'],
     )
-    spawn_entity = Node(package='gazebo_ros',
-                        executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'ugv'],
-                        output='screen')
-    # launch.logging.launch_config.level = logging.WARN
-    return [gazebo, spawn_entity]
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'],
+        output='screen'
+    )
+    return [gzserver_cmd, gz_spawn_entity,bridge]
--- ugv_description/setup.py
+++ ugv_description/setup.py
@@ -17,6 +17,7 @@ setup(
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
