0232ecb53fc25845a4d7795d86a9e0300b579208
Omar Salem <omar.gamil.salem@gmail.com>
2024-07-28 20:00:19 +1000

    removed steps from ugv_mapping

--- ugv_mapping/ugv_mapping_install.sh
+++ ugv_mapping/ugv_mapping_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-ls /dev/tty* | grep ttyUSB1 #check /dev/ttyUSB1
-cd sllidar_ros2/scripts
-source create_udev_rules.sh
-ros2 launch sllidar_ros2 view_sllidar_c1_launch.py
-
 cd ~/ugv_ws
 colcon build --packages-select ugv_mapping
 source install/setup.bash
