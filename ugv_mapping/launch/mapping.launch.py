394f1646e4f790d6fb49caad989d5360d4118fdb
Omar Salem <omar.gamil.salem@gmail.com>
2024-07-29 14:42:41 +1000

    formatting

--- ugv_mapping/launch/mapping.launch.py
+++ ugv_mapping/launch/mapping.launch.py
@@ -10,35 +10,40 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    package_name = 'ugv_mapping'
+    package_name = "ugv_mapping"
 
     rp_lidar_c1_launch_file_path = PathJoinSubstitution(
-        [FindPackageShare('sllidar_ros2'), 'launch', 'sllidar_c1_launch.py'])
+        [FindPackageShare("sllidar_ros2"), "launch", "sllidar_c1_launch.py"]
+    )
     rp_lidar_c1 = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(rp_lidar_c1_launch_file_path)
     )
 
     core = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'core.launch.py'
-        )]), launch_arguments={
-            'package_name': package_name,
-            'is_sim': 'False'
-        }.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "core.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={"package_name": package_name, "is_sim": "False"}.items(),
     )
     package_dir = FindPackageShare(package_name)
-    rviz_config_file = PathJoinSubstitution(
-        [package_dir, 'config', 'display.rviz']
+    rviz_config_file = PathJoinSubstitution([package_dir, "config", "display.rviz"])
+
+    control = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("ugv_control"),
+                    "launch",
+                    "control.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={"rviz_config_file": rviz_config_file}.items(),
     )
-    return LaunchDescription(
-        [core,
-         rp_lidar_c1,
-         IncludeLaunchDescription(
-             PythonLaunchDescriptionSource([os.path.join(
-                 get_package_share_directory('ugv_control'), 'launch', 'control.launch.py'
-             )]), launch_arguments={
-                 'rviz_config_file': rviz_config_file
-             }.items()
-         )
-         ]
-    )
+    return LaunchDescription([core, rp_lidar_c1, control])
--- ugv_mapping/ugv_mapping_install.sh
+++ ugv_mapping/ugv_mapping_install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cd ~/ugv_ws
-colcon build --packages-select ugv_mapping
-source install/setup.bash
-ros2 launch ugv_mapping mapping.launch.py
+cd ~/ugv_ws && colcon build --packages-select ugv_mapping && source install/setup.bash && ros2 launch ugv_mapping mapping.launch.py
 
 cd ~/ugv_ws
 colcon build --packages-select ugv_mapping
