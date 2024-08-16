04ef05818e9fee12846a4fd253a891a4c2d4010b
Omar Salem <omar.gamil.salem@gmail.com>
2024-08-16 22:04:11 +1000

    gazebo

--- ugv_description/launch/gazebo.launch.py
+++ ugv_description/launch/gazebo.launch.py
@@ -13,57 +13,79 @@ from launch.substitutions import Command, PathJoinSubstitution, LaunchConfigurat
 
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('ugv_description')
-    xacro_file = os.path.join(share_dir, 'urdf', 'ugv.xacro')
-    robot_description_config = xacro.process_file(xacro_file, mappings={'is_sim': 'true'})
-    robot_urdf = robot_description_config.toprettyxml(indent='  ')
+    share_dir = get_package_share_directory("ugv_description")
+    xacro_file = os.path.join(share_dir, "urdf", "ugv.xacro")
+    robot_description_config = xacro.process_file(
+        xacro_file, mappings={"is_sim": "true"}
+    ).toprettyxml(indent="  ")
 
-    return LaunchDescription([
-            DeclareLaunchArgument(
-                name='world',
-                default_value=''
+    robot_urdf_arg = DeclareLaunchArgument(
+        name="robot_urdf", default_value=robot_description_config
+    )
+    robot_urdf = LaunchConfiguration("robot_urdf")
+
+    return LaunchDescription(
+        
+        [robot_urdf_arg,
+            DeclareLaunchArgument(name="world", default_value=os.path.join(
+        get_package_share_directory('ugv_description'),
+        'worlds',
+        'empty_world.world'
+    )),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [os.path.join(share_dir, "launch"), "/display.launch.py"]
+                ),
+                launch_arguments={"use_sim_time": "True"}.items(),
             ),
-        IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(share_dir, 'launch'), '/display.launch.py']),
-        launch_arguments={'use_sim_time': 'True'}.items()
-         
-             ),
-    ]+create_gazebo_nodes(robot_urdf))
+        ]
+        + create_gazebo_nodes(robot_urdf),
+    )
+
 
 def create_gazebo_nodes(robot_urdf) -> list:
     """
 
     :rtype: list
-    """    
+    """
+
+    ros_gz_sim = get_package_share_directory("ros_gz_sim")
     
-    ros_gz_sim =get_package_share_directory('ros_gz_sim')
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     ros_gz_sim, 'launch'), '/gz_sim.launch.py']),
     launch_arguments={'gz_args': ['-v4 ', LaunchConfiguration('world')], 'on_exit_shutdown': 'true'}.items()
              )
+    
+    # gzserver_cmd = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+    #     ),
+    #     launch_arguments={'gz_args': ['-r -s -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+    # )
+    # gzclient_cmd = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+    #     ),
+    #     launch_arguments={'gz_args': '-g -v4 '}.items()
+    # )
+
 
     gz_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=['-string', robot_urdf,
-                   '-x', '0.0',
-                   '-y', '0.0',
-                   '-z', '0.07',
-                   '-R', '0.0',
-                   '-P', '0.0',
-                   '-Y', '0.0',
-                   '-name', 'fws_robot',
-                   '-allow_renaming', 'false'],
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-string", robot_urdf,
+            "-name","ugv",
+        ],
     )
 
-    # Bridge
     bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'],
-        output='screen'
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=["/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan"],
+        output="screen",
     )
 
-    return [gazebo,gz_spawn_entity,bridge]
+    return [gazebo, gz_spawn_entity,bridge]
