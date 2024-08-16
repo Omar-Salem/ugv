ccfa39775620d3a380454517d6d71dad77d45f75
Omar Salem <omar.gamil.salem@gmail.com>
2024-08-16 19:08:36 +1000

    ugv_description c++ package

--- ugv_description/launch/display.launch.py
+++ ugv_description/launch/display.launch.py
@@ -69,9 +69,10 @@ def generate_launch_description():
 
     return LaunchDescription([
         gui_arg,
+        robot_urdf_arg,
         rviz_config_file_arg,
+        rviz_node,
         robot_state_publisher_node,
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
-        rviz_node
+        joint_state_publisher_gui_node
     ])
