66233fdd4d353743d5070df37a77c1541a512578
Omar Salem <omar.gamil.salem@gmail.com>
2024-09-01 00:23:27 +1000

    refactored gazebo laynch file to use description package

--- ugv_control/launch/gazebo.launch.py
+++ ugv_control/launch/gazebo.launch.py
@@ -8,53 +8,42 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    package_name = 'ugv_control'
+    share_dir = get_package_share_directory(package_name)
+    xacro_file = os.path.join(share_dir, 'urdf', 'ugv.xacro')
+    robot_description_config = xacro.process_file(xacro_file, mappings={'is_sim': 'true'})
+    robot_urdf = robot_description_config.toxml()
 
-    # Get URDF via xacro
-    ugv_control_pkg = FindPackageShare('ugv_control')
-    robot_description_content = Command(
+
+
+    controller_nodes = create_controller_nodes()
+
+    return LaunchDescription(
         [
-            PathJoinSubstitution([FindExecutable(name='xacro')]),
-            ' ',
-            PathJoinSubstitution(
-                [ugv_control_pkg,
-                 'urdf', 'ugv.xacro']
-            ),
-        ]
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('ugv_description'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments={ 'robot_urdf': robot_urdf}.items()
+         
+             )
+        ] +
+        controller_nodes
     )
-    robot_description = {'robot_description': robot_description_content}
+
+
+def create_controller_nodes() -> list:
+    """
+
+    :rtype: list
+    """
     robot_controllers = PathJoinSubstitution(
         [
-            ugv_control_pkg,
+            FindPackageShare('ugv_control'),
             'config',
             'diff_drive_controllers.yaml',
         ]
-    )
-
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
-    )
-
-    gz_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=['-topic', 'robot_description', '-name',
-                   'diff_drive', '-allow_renaming', 'true'],
     )
 
     joint_state_broadcaster_spawner = Node(
@@ -62,7 +51,7 @@ def generate_launch_description():
         executable='spawner',
         arguments=['joint_state_broadcaster'],
     )
-    diff_drive_base_controller_spawner = Node(
+    diff_drive_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
         arguments=[
@@ -71,36 +60,4 @@ def generate_launch_description():
             robot_controllers,
             ],
     )
-    world=os.path.join(
-        get_package_share_directory('ugv_description'),
-        'worlds',
-        'empty_world'
-    )
-
-    return LaunchDescription([
-        # Launch gazebo environment
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
-                                       'launch',
-                                       'gz_sim.launch.py'])]),
-            launch_arguments=[('gz_args', [world,
-                                 '.sdf',
-                                 ' -v 4',
-                                 ' -r']
-                    )]),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=gz_spawn_entity,
-                on_exit=[joint_state_broadcaster_spawner],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster_spawner,
-                on_exit=[diff_drive_base_controller_spawner],
-            )
-        ),
-        node_robot_state_publisher,
-        gz_spawn_entity
-    ])
+    return [joint_state_broadcaster_spawner, diff_drive_controller_spawner]
