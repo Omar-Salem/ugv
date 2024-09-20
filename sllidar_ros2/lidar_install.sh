9cd0ec017f9a49cb1082fcfb260dd7434f810757
Omar Salem <omar.gamil.salem@gmail.com>
2024-09-20 19:13:18 +1000

    fixing lidar run issue

--- sllidar_ros2/lidar_install.sh
+++ sllidar_ros2/lidar_install.sh
@@ -1,3 +1,3 @@
 ls /dev/tty* | grep ttyUSB1 #check /dev/ttyUSB1
 sudo chmod 777 /dev/ttyUSB1
-cd ~/ugv_ws && colcon build --packages-select sllidar_ros2 && source install/setup.bash && ros2 launch sllidar_ros2 view_sllidar_c1_launch.py
+cd ~/ugv_ws && colcon build --packages-select sllidar_ros2 && source install/setup.bash && ros2 launch sllidar_ros2 view_sllidar_c1.launch.py
