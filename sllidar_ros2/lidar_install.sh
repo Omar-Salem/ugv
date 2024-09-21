4f8a34ab4623c6de8c019bba762c7d95d9724bd7
Omar Salem <omar.gamil.salem@gmail.com>
2024-09-21 11:21:49 +1000

    lidar scripts

--- sllidar_ros2/lidar_install.sh
+++ sllidar_ros2/lidar_install.sh
@@ -1,3 +1,6 @@
-ls /dev/tty* | grep ttyUSB1 #check /dev/ttyUSB1
+ls /dev/ttyUSB* | grep ttyUSB1 #check /dev/ttyUSB1
 sudo chmod 777 /dev/ttyUSB1
+cd ~/ugv_ws/src/sllidar_ros2/scripts
+chmod +x create_udev_rules.sh
+./create_udev_rules.sh
 cd ~/ugv_ws && colcon build --packages-select sllidar_ros2 && source install/setup.bash && ros2 launch sllidar_ros2 view_sllidar_c1.launch.py
