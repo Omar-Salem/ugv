efa1eaac64e293477ee5e40bb50454e9951d7641
Omar Salem <omar.gamil.salem@gmail.com>
2024-09-28 08:51:28 +1000

    fixed /diff_drive_controller/cmd_vel pub command

--- ugv_control/ugv_control_install.sh
+++ ugv_control/ugv_control_install.sh
@@ -6,8 +6,7 @@ cd ~/ugv_ws && colcon build --packages-select ugv_control && source install/setu
 cd ~/ugv_ws && colcon build --packages-select ugv_control && source install/setup.bash && ros2 launch ugv_control gazebo.launch.py
 
 # terminal 2
-# NOT WORKING
-ros2 topic pub --once /diff_drive_controller/cmd_vel geometry_msgs/msg/TwistStamped "{header: auto}, twist: {linear: {x: 0.2, y: 0.0, z: 0.0}}}"
+ros2 topic pub -r 10 /diff_drive_controller/cmd_vel geometry_msgs/msg/TwistStamped "{header: auto, twist: {linear: {x: 0.1}}}"
 
 
 ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=/diff_drive_controller/cmd_vel -p stamped:=True
