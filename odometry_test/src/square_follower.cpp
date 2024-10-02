4bc90243fb0eea52e56965afdcb9320728b6b354
Omar Salem <omar.gamil.salem@gmail.com>
2024-10-02 10:38:38 +1000

    turning

--- odometry_test/src/square_follower.cpp
+++ odometry_test/src/square_follower.cpp
@@ -4,9 +4,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
-
+#include "nav_msgs/msg/odometry.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 using namespace std::chrono_literals;
 using geometry_msgs::msg::TwistStamped;
+using nav_msgs::msg::Odometry;
 
 class SquareFollower : public rclcpp::Node
 {
@@ -14,23 +16,69 @@ public:
   SquareFollower()
       : Node("square_follower"), count_(0)
   {
-    velocity_publisher_ = this->create_publisher<TwistStamped>("/diff_drive_controller/cmd_vel", 10);
+    velocityPublisher_ = this->create_publisher<TwistStamped>("/diff_drive_controller/cmd_vel", 10);
     auto timer_callback =
         [this]() -> void
     {
       count_++;
       auto message = TwistStamped();
       message.header.stamp = this->now();
-      message.twist.linear.x = 0.1;
-      // RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      this->velocity_publisher_->publish(message);
+
+      if (count_ % 100 == 0)
+      {
+        turning_ = true;
+      }
+      if (turning_)
+      {
+        message.twist.angular.z = 0.1;
+      }
+      else
+      {
+        message.twist.linear.x = 0.1;
+      }
+      this->velocityPublisher_->publish(message);
+
+      auto turnAngle = abs(startYaw_ - currentYaw_);
+      RCLCPP_INFO(this->get_logger(), "turnAngle: '%f'", turnAngle);
+      RCLCPP_INFO(this->get_logger(), "currentYaw_: '%f'", currentYaw_);
+      if (turning_ && turnAngle >= 1.5708)
+      {
+        turning_ = false;
+        startYaw_ = currentYaw_;
+      }
     };
+
+    auto topic_callback =
+        [this](Odometry::UniquePtr odom) -> void
+    {
+      auto orientation = odom->pose.pose.orientation;
+      tf2::Quaternion quat_tf;
+      tf2::fromMsg(orientation, quat_tf);
+      double r{}, p{}, y{};
+      tf2::Matrix3x3 m(quat_tf);
+      m.getRPY(r, p, y);
+      if (!yawInitialized_)
+      {
+        startYaw_ = y;
+        yawInitialized_ = true;
+      }
+      currentYaw_ = y;
+      // RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    };
+    odometrySubscription_ =
+        this->create_subscription<Odometry>("/diff_drive_controller/odom", 10, topic_callback);
+
     timer_ = this->create_wall_timer(100ms, timer_callback);
   }
 
 private:
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<TwistStamped>::SharedPtr velocity_publisher_;
+  rclcpp::Publisher<TwistStamped>::SharedPtr velocityPublisher_;
+  rclcpp::Subscription<Odometry>::SharedPtr odometrySubscription_;
+  double startYaw_;
+  double currentYaw_;
+  bool yawInitialized_;
+  bool turning_;
   size_t count_;
 };
 
