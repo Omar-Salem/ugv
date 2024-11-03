dae9b52a45eeadf6bc42f307311dd91cc00286f4
Omar Salem <omar.gamil.salem@gmail.com>
2024-11-03 15:38:47 +1100

    increased nav min velocity set mapping mode to mapping

--- ugv_mapping/ugv_mapping_install.sh
+++ ugv_mapping/ugv_mapping_install.sh
@@ -6,3 +6,6 @@ cd ~/ugv_ws && colcon build --packages-select ugv_mapping && source install/setu
 
 # Sim
 cd ~/ugv_ws && colcon build --packages-select ugv_mapping && source install/setup.bash && ros2 launch ugv_mapping gazebo.launch.py
+
+
+ros2 topic pub --once /diff_drive_controller/odom nav_msgs/msg/Odometry "{header: {frame_id: 'odom'},child_frame_id: 'base_link', pose: {pose: {orientation: {x: 0.0,y: 0.0,z: 1.0,w: 0.0}}}}"
