515e1adc1e1bff6c0018cfa5aea1970d01c258b9
Omar Salem <omar.gamil.salem@gmail.com>
2024-11-09 08:29:53 +1100

    setting use_sim_time for all nodes in ugv_control gazbeo launch file

--- ugv_control/launch/gazebo.launch.py
+++ ugv_control/launch/gazebo.launch.py
@@ -3,10 +3,10 @@ import os
 import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 from launch_ros.substitutions import FindPackageShare
 
 
@@ -25,30 +25,34 @@ def generate_launch_description():
         os.path.join(get_package_share_directory("ugv_description"), "launch"),
         "/gazebo.launch.py",
     ]
-    
+
     rviz_config_file_arg = DeclareLaunchArgument(
-                name="rviz_config_file",
-                default_value=os.path.join(share_dir, "config", "display.rviz"),
-            )
+        name="rviz_config_file",
+        default_value=os.path.join(share_dir, "config", "display.rviz"),
+    )
     return LaunchDescription(
         [
-    #         Node(
-    #     package="odometry_test",
-    #     executable="square_follower",
-    # ),
-    
             rviz_config_file_arg,
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(ugv_description_launch),
-                launch_arguments={
-                                "robot_urdf": robot_urdf, 
-                                  "use_gui": "True",
-                                  "use_joint_state_publisher": "False",
-                                    "rviz_config_file": LaunchConfiguration("rviz_config_file"),
-                                  }.items(),
-            )
+            GroupAction(
+                actions=[
+                    SetParameter(name="use_sim_time", value=True),
+                    #         Node(
+                    #     package="odometry_test",
+                    #     executable="square_follower",
+                    # ),
+                    IncludeLaunchDescription(
+                        PythonLaunchDescriptionSource(ugv_description_launch),
+                        launch_arguments={
+                            "robot_urdf": robot_urdf,
+                            "use_gui": "True",
+                            "use_joint_state_publisher": "False",
+                            "rviz_config_file": LaunchConfiguration("rviz_config_file"),
+                        }.items(),
+                    ),
+                ]
+                + controller_nodes
+            ),
         ]
-        + controller_nodes
     )
 
 
