d3fbefd69713e237e6385cacd0fee7027df3dfaa
Omar Salem <omar.gamil.salem@gmail.com>
2024-11-10 09:18:25 +1100

    ros2 control Sanity check

--- ugv_control/ugv_control_install.sh
+++ ugv_control/ugv_control_install.sh
@@ -9,8 +9,8 @@ cd ~/ugv_ws && colcon build --packages-select ugv_control && source install/setu
 # Sim
 cd ~/ugv_ws && colcon build --packages-select ugv_control && source install/setup.bash && ros2 launch ugv_control gazebo.launch.py
 
-# terminal 2
-ros2 topic pub -r 10 /diff_drive_controller/cmd_vel geometry_msgs/msg/TwistStamped "{header: auto, twist: {linear: {x: 0.1}}}"
+# Sanity check
+ros2 topic pub --once /diff_drive_controller/cmd_vel geometry_msgs/msg/TwistStamped "{header: auto, twist: {linear: {x: 0.1}}}"
 
 
 ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=/diff_drive_controller/cmd_vel -p stamped:=True
