60ae48e8c0dbf0ef788c85653520b265e43481d5
Omar Salem <omar.gamil.salem@gmail.com>
2024-11-23 11:48:55 +1100

    updated control sanity check to rotate in place

--- ugv_control/ugv_control_install.sh
+++ ugv_control/ugv_control_install.sh
@@ -10,7 +10,7 @@ cd ~/ugv_ws && colcon build --packages-select ugv_control && source install/setu
 cd ~/ugv_ws && colcon build --packages-select ugv_control && source install/setup.bash && ros2 launch ugv_control gazebo.launch.py
 
 # Sanity check
-ros2 topic pub --once /diff_drive_controller/cmd_vel geometry_msgs/msg/TwistStamped "{header: auto, twist: {linear: {x: 0.1}}}"
+ros2 topic pub /diff_drive_controller/cmd_vel geometry_msgs/msg/TwistStamped "{header: auto, twist: {angular: {z: 0.1}}}"
 
 
 ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=/diff_drive_controller/cmd_vel -p stamped:=True
